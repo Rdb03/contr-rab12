fba68a36ae7fd430c076d084916c6298b81b507a
rdb03 <rdb03@mail.ru>
2024-03-30 11:45:11 +0600

    Установил все нужные библеотеки для начала проекта. Сделал рагистрацию, логин и логаут с прошлых занятий на backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import {Container} from "@mui/material";
+import {Routes} from "react-router-dom";
+
+const App = () => {
+
+    return (
+        <>
+            <header>
+            </header>
+            <main className="app-main">
+                <Container maxWidth="xl" sx={{marginTop: '50px'}}>
+                    <Routes>
+
+                    </Routes>
+                </Container>
+            </main>
+        </>
+    );
+};
+
+export default App
