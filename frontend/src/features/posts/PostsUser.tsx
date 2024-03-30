20449de7bb15b117063cacd41442bac266252d34
rdb03 <rdb03@mail.ru>
2024-03-30 18:42:32 +0600

    Если страница юзера пустая, ему предлагается добавить пост

--- frontend/src/features/posts/PostsUser.tsx
+++ frontend/src/features/posts/PostsUser.tsx
@@ -37,36 +37,68 @@ const PostsUser = () => {
     };
 
     return loading ? (
-        <CircularProgress/>
+        <CircularProgress />
     ) : (
-        <Grid>
-            {modal ? <Modal onClose={onClose} image={modalImage} /> : ''}
-            <Grid sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-                <Typography sx={{color: 'white'}} variant="h3">{oneUser?.displayName} Gallery</Typography>
-                {userId === user?._id ?     <Link
-                    to={`/new_post`}
-                    style={{
-                        color: 'black',
-                        textDecoration: 'none',
-                    }}
-                >
-                    <Typography sx={{
-                        color: 'white',
-                        fontWeight: 'bold',
-                        fontSize: '30px'
-                    }}>Add new Post</Typography>
-                </Link> : null}
-            </Grid>
-            <Grid sx={{display: 'flex', marginTop: '30px'}}>
-                {postsUser.map((item) => (
-                    (user?.role === 'admin' || user?._id === item.user?._id || item.published) && (
-                        <PostItem modal={() => {
-                            setModalImage(item.image);
-                            setModal(true);
-                        }} post={item} key={item._id}/>
-                    )
-                ))}
-            </Grid>
+        <Grid sx={{display: 'flex', flexDirection: 'column'}}>
+            {postsUser.length > 0 ? (
+                <>
+                    {modal && <Modal onClose={onClose} image={modalImage} />}
+                    <Grid sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography sx={{ color: 'white' }} variant="h3">{oneUser?.displayName} Gallery</Typography>
+                        {userId === user?._id && (
+                            <Link
+                                to={`/new_post`}
+                                style={{
+                                    color: 'black',
+                                    textDecoration: 'none',
+                                }}
+                            >
+                                <Typography sx={{ color: 'white', fontWeight: 'bold', fontSize: '30px' }}>
+                                    Add new Post
+                                </Typography>
+                            </Link>
+                        )}
+                    </Grid>
+                    <Grid sx={{ display: 'flex', marginTop: '30px' }}>
+                        {postsUser.map((item) => (
+                            (user?.role === 'admin' || user?._id === item.user?._id || item.published) && (
+                                <PostItem
+                                    modal={() => {
+                                        setModalImage(item.image);
+                                        setModal(true);
+                                    }}
+                                    post={item}
+                                    key={item._id}
+                                />
+                            )
+                        ))}
+                    </Grid>
+                </>
+            ) : (
+                <Grid sx={{display: 'flex', flexDirection: 'column', textAlign: 'center'}}>
+                    <Typography variant="h2" sx={{color: 'white', margin: '0 auto'}}>У вас нет никаких публикаций.</Typography>
+                    {userId === user?._id && (
+                        <Link
+                            to={`/new_post`}
+                            style={{
+                                color: 'black',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            <Typography variant="h3" sx={{
+                                color: 'Green',
+                                fontWeight: 'bold',
+                                fontSize: '30px',
+                                backgroundColor:'white',
+                                width: '200px',
+                                margin: '70px auto'
+                            }}>
+                                Add new Post
+                            </Typography>
+                        </Link>
+                    )}
+                </Grid>
+            )}
         </Grid>
     );
 };
